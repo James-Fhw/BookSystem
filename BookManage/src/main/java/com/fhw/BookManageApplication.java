0a2e89d9e7cd7985cc89c11802a4ad7793eaf348
fhw <1337192253@qq.com>
2022-10-29 15:59:16 +0800

    bookManage-commit

--- BookManage/src/main/java/com/fhw/BookManageApplication.java
+++ BookManage/src/main/java/com/fhw/BookManageApplication.java
@@ -8,5 +8,6 @@ public class BookManageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookManageApplication.class, args);
+        System.out.println(123);
     }
 }
